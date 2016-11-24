b29b93a9ff444521e5f6a8404f8acc8c26cd4594
SabatoRusso <russosabato88@gmail.com>
2016-11-24 08:34:31 +0100

    Initial Commit

---
+++ US Real San Felice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  US Real San Felice
+//
+//  Created by Russo Sabato (Italdata spa) on 24/11/16.
+//  Copyright © 2016 Russo Sabato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
