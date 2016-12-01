bbbcedf69d581412fd3e9b99334919b9a4aab276
sabat.russo <russosabato88@gmail.com>
2016-12-01 20:22:04 +0100

    Eliminato pods

--- Pods/Target Support Files/Pods-US Real San Felice/Pods-US Real San Felice-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_US_Real_San_Felice : NSObject
-@end
-@implementation PodsDummy_Pods_US_Real_San_Felice
-@end
--- Pods/Target Support Files/Pods-US Real San Felice/Pods-US Real San Felice-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_US_Real_San_FeliceVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_US_Real_San_FeliceVersionString[];
-
